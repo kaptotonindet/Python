43d041b01219b2c97df6b7b0ad5e77ae1d0bbe0b
kaptotonindet <kaptotonindet@gmail.com>
2021-02-25 12:59:25 +0000

    program

--- first.py
+++ first.py
@@ -1,2 +1,32 @@
 print("Hello World")
+# for x in "banana": 
+#     print(x)
+
+# a=['peter','kiplangat','Towett']
+# for a in a:
+#     print(a)
+
+# #print(a)
+# b=[20,31,76,32]
+# sum=0
+# for elements in b:
+#     sum=sum+elements
+# print(sum)
+# # c= list(range(1,11))
+# # print(c)
+# total=0
+# for i in range(1,11):
+#     total=total + i
+
+# print(total)
+# for i in range(1,100):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(f'{i} is a multiple of 3 and 5!')
+d=[7,5,4,4,3,1,-2,-3, -5,-7]
+sum = 0
+for i in d:
+    if i<0:
+        sum=sum+i
+
+print(sum)
 
